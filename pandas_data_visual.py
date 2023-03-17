ee5ac02585763872393fc6fc163d28c0952fa2e1
Luka <lukianeshavladze@gmail.com>
2023-03-17 04:26:14 +0400

    pandas_vis

---
+++ pandas_data_visual.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df1 = pd.read_csv('df1', index_col=0)
+print(df1.head())
+
+df2 = pd.read_csv('df2')
+print(df2.head())
+
+# making histogram with pandas
+df1['A'].hist(bins=30)
+plt.show()
